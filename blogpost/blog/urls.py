43c51e9976f1089f5646ee17cb5faacacf20ff02
gatayaMed <gataya.mohammed@gmail.com>
2025-02-04 13:02:14 +0100

    addpost created in views , urls  and in templates folder

--- blogpost/blog/urls.py
+++ blogpost/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from . views import HomeView,PostDetails
+from . views import HomeView,PostDetails,AddPostView
 urlpatterns = [
     
      #path('', views.home, name='home'),
      path('',HomeView.as_view(), name='home' ),
      path('article/<int:pk>',PostDetails.as_view(), name='postdetails' ),
+     path('addpost/',AddPostView.as_view(), name='addpost' ),
 
     
      
--- blogpost/blog/views.py
+++ blogpost/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from . models import Post
-from django.views.generic import ListView,DetailView
-
+from django.views.generic import ListView,DetailView,CreateView
+from .forms import PostForm 
+from django.urls import reverse_lazy
 # Create your views here.
 #def home(request):
    # return  render (request, 'home.html', {})
@@ -17,3 +18,14 @@ class HomeView(ListView):
 class PostDetails(DetailView):
     model = Post
     template_name= 'postdetails.html'
+
+
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm # we use this line form_class when we want to show the fields  from our forms.py not automatic 
+    template_name = 'addpost.html'
+    success_url = reverse_lazy('home')  # Change 'home' to the actual name of the page you want to redirect to
+    #fields = '__all__'
+    #  here instead of showing all the fields in the post model  we can do it manually like that :
+    # fields = ('title', 'body')
