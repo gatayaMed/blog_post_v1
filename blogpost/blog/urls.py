3717b8cfed657a9712cba111df6f3ffe4ea7d474
gatayaMed <gataya.mohammed@gmail.com>
2025-02-04 12:30:19 +0100

    creating the postdetails view and template

--- blogpost/blog/urls.py
+++ blogpost/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from . views import HomeView
+from . views import HomeView,PostDetails
 urlpatterns = [
     
      #path('', views.home, name='home'),
      path('',HomeView.as_view(), name='home' ),
+     path('article/<int:pk>',PostDetails.as_view(), name='postdetails' ),
 
     
      
--- blogpost/blog/views.py
+++ blogpost/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
 #def home(request):
@@ -11,3 +11,9 @@ class HomeView(ListView):
     template_name = 'home.html'
     #ordering = ['-id'] # order post by negative id 
     ordering = ['-post_date']  #  ordering the posts by date   
+
+
+
+class PostDetails(DetailView):
+    model = Post
+    template_name= 'postdetails.html'
