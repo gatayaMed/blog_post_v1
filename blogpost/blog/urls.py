b69194739a739216b1bb9c07df34a8110e96c2f0
gatayaMed <gataya.mohammed@gmail.com>
2025-02-04 11:22:32 +0100

    initial settings to our project blog_post_v1

---
+++ blogpost/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from . views import home
+urlpatterns = [
+    
+    #path('', views.home, name='home'),
+     path('',home, name='home' ),
+     
+]
