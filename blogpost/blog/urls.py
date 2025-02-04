b31b4ef3132b1621cef668a9482c67a97c3d60b2
gatayaMed <gataya.mohammed@gmail.com>
2025-02-04 13:14:58 +0100

    i have add the updatepost in urls, views and updatepost.html in templates folder, to update any posts

--- blogpost/blog/urls.py
+++ blogpost/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from . views import HomeView,PostDetails,AddPostView
+from . views import HomeView,PostDetails,AddPostView,UpdatePostView
 urlpatterns = [
     
      #path('', views.home, name='home'),
      path('',HomeView.as_view(), name='home' ),
      path('article/<int:pk>',PostDetails.as_view(), name='postdetails' ),
      path('addpost/',AddPostView.as_view(), name='addpost' ),
+     path('article/edit/<int:pk>',UpdatePostView.as_view(), name='updatepost' ),
 
     
      
--- blogpost/blog/views.py
+++ blogpost/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from . models import Post
-from django.views.generic import ListView,DetailView,CreateView
-from .forms import PostForm 
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from .forms import PostForm ,EditForm
 from django.urls import reverse_lazy
 # Create your views here.
 #def home(request):
@@ -25,7 +25,15 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm # we use this line form_class when we want to show the fields  from our forms.py not automatic 
     template_name = 'addpost.html'
-    success_url = reverse_lazy('home')  # Change 'home' to the actual name of the page you want to redirect to
+    success_url = reverse_lazy('home')  # ensures that Django knows where to redirect after a successful form submission.
     #fields = '__all__'
     #  here instead of showing all the fields in the post model  we can do it manually like that :
     # fields = ('title', 'body')
+
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'updatepost.html'
+    success_url = reverse_lazy('home')  # Change 'home' to the actual name of the page you want to redirect to
+    #fields = ['title','title_tag','body']
